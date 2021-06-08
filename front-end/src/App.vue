977390bb9cb49639dcf3630d8c95308dea7da909
Lorena Iannicelli <lai.iannicelli@gmail.com>
2021-06-08 14:46:42 -0500

    add front end

---
+++ front-end/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div id="nav"></div>
+    <router-view />
+    <div id="footer">
+      <p>I spent __ hours on this website</p>
+      <p><a href="https://github.com/LorenaIannicelli/Scopa"> Github! </a></p>
+      <p>Lorena Iannicelli, computer engineering</p>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+#footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+</style>
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div id="welcome">
+      <p>Ciao!</p>
+      <p>Benevunti a Scopa ~~ Welcome to Scopa!</p>
+    </div>
+    <div id="funFacts">
+      <p>Scopa Fun Facts</p>
+    </div>
+    <div id="howToPlay">
+      <p>How to play Scopa</p>
+    </div>
+    <div id="play">
+      <p>Click here to play!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+}
+</style>
